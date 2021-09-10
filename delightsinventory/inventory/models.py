a048c6282e6fedaa1c175401791194d519cb5008
Jou Xiong <jouxiong15@gmail.com>
2021-09-10 18:45:01 -0500

    models display on admin page

--- delightsinventory/inventory/models.py
+++ delightsinventory/inventory/models.py
@@ -6,14 +6,23 @@ class Recipe(models.Model):
     ingredient = models.CharField(max_length=50)
     cost = models.IntegerField(default=0)
 
+    def get_absolute_url(self):
+        return '/recipe/list'
 
-class Ingredients(models.Model):
-    food = models.CharField(max_length=50)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
     unit_price = models.IntegerField(default=0)
     inventory = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return '/ingredient/list'
 
 
 class Purchase(models.Model):
     menu_item = models.CharField(max_length=100)
     date = models.DateField(auto_now=False, auto_now_add=False)
     time = models.TimeField(auto_now=False, auto_now_add=False)
+
+    def get_absolute_url(self):
+        return '/purchase/list'

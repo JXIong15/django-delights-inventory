85f6be8c41850ad3f7a0cbafeee4c93077934d68
Jou Xiong <jouxiong15@gmail.com>
2021-09-10 19:16:09 -0500

    added forms

--- delightsinventory/inventory/urls.py
+++ delightsinventory/inventory/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
   path("", views.home, name="home"),
+
+  path("recipe/list", views.RecipeList.as_view(), name="recipelist"),
+#   path("recipe/create", views.RecipeCreate.as_view(), name="recipecreate"),
+#   path("recipe/update/<pk>", views.RecipeUpdate.as_view(), name="recipeupdate"),
+  path("recipe/delete/<pk>", views.RecipeDelete.as_view(), name="recipedelete"),
+  path("ingredient/list", views.IngredientList.as_view(), name="ingredientlist"),
+#   path("ingredient/create", views.IngredientCreate.as_view(), name="ingredientcreate"),
+#   path("ingredient/update/<pk>", views.IngredientUpdate.as_view(), name="ingredientupdate"),
+  path("ingredient/delete/<pk>", views.IngredientDelete.as_view(), name="ingredientdelete"),
+  path("purchase/list", views.PurchaseList.as_view(), name="purchaselist"),
+#   path("purchase/create", views.PurchaseCreate.as_view(), name="purchasecreate"),
+#   path("purchase/update/<pk>", views.PurchaseUpdate.as_view(), name="purchaseupdate"),
+  path("purchase/delete/<pk>", views.PurchaseDelete.as_view(), name="purchasedelete"),
 ]
--- delightsinventory/inventory/views.py
+++ delightsinventory/inventory/views.py
@@ -4,8 +4,8 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-# from .models import Owner, Patient, Appointment
-# from .forms import OwnerCreateForm, OwnerUpdateForm, PatientCreateForm, PatientUpdateForm, AppointmentCreateForm, AppointmentUpdateForm
+from .models import Recipe, Ingredient, Purchase
+# from .forms import RecipeCreateForm, RecipeUpdateForm, IngredientCreateForm, IngredientUpdateForm, PurchaseCreateForm, PurchaseUpdateForm
 
 # from django.urls import reverse_lazy
 # from django.contrib.auth.forms import UserCreationForm
@@ -15,3 +15,62 @@ def home(request):
 #   context = {"name": request.user}
 # return render(request, "inventory/home.html", context)
   return render(request, "inventory/home.html")
+
+class RecipeList(ListView):
+    model = Recipe
+
+class IngredientList(ListView):
+    model = Ingredient
+
+class PurchaseList(ListView):
+    model = Purchase
+
+
+# class RecipeCreate(LoginRequiredMixin, CreateView):
+#   model = Recipe
+#   template_name = "vetoffice/Recipe_create_form.html"
+#   form_class = RecipeCreateForm
+
+# class IngredientCreate(LoginRequiredMixin, CreateView):
+#   model=Ingredient
+#   template_name = "vetoffice/Ingredient_create_form.html"
+#   form_class = IngredientCreateForm
+
+# class PurchaseCreate(LoginRequiredMixin, CreateView):
+#   model=Purchase
+#   template_name = "vetoffice/Purchase_create_form.html"
+#   form_class = PurchaseCreateForm
+
+#   def form_valid(self, form):
+#     form.instance.user = self.request.user
+#     return super().form_valid(form)
+
+# class RecipeUpdate(LoginRequiredMixin, UpdateView):
+#   model = Recipe
+#   template_name = "vetoffice/Recipe_update_form.html"
+#   form_class = RecipeUpdateForm
+
+# class IngredientUpdate(LoginRequiredMixin, UpdateView):
+#   model = Ingredient
+#   template_name = "vetoffice/Ingredient_update_form.html"
+#   form_class = IngredientUpdateForm
+
+# class PurchaseUpdate(LoginRequiredMixin, UpdateView):
+#   model = Purchase
+#   template_name = "vetoffice/Purchase_update_form.html"
+#   form_class = PurchaseUpdateForm
+
+class RecipeDelete(LoginRequiredMixin, DeleteView):
+  model = Recipe
+  template_name = "vetoffice/Recipe_delete_form.html"
+  success_url = "/Recipe/list"
+
+class IngredientDelete(LoginRequiredMixin, DeleteView):
+  model = Ingredient
+  template_name = "vetoffice/Ingredient_delete_form.html"
+  success_url = "/Ingredient/list"
+
+class PurchaseDelete(LoginRequiredMixin, DeleteView):
+  model = Purchase
+  template_name = "vetoffice/Purchase_delete_form.html"
+  success_url = "/Purchase/list"

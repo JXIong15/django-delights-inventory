083e30d60cbea9bacef6642b6e04e1b49603ddf4
Jou Xiong <jouxiong15@gmail.com>
2021-09-10 19:36:58 -0500

    added list html, view classes, and urls

--- delightsinventory/inventory/views.py
+++ delightsinventory/inventory/views.py
@@ -5,9 +5,9 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe, Ingredient, Purchase
-# from .forms import RecipeCreateForm, RecipeUpdateForm, IngredientCreateForm, IngredientUpdateForm, PurchaseCreateForm, PurchaseUpdateForm
+from .forms import RecipeCreateForm, RecipeUpdateForm, IngredientCreateForm, IngredientUpdateForm, PurchaseCreateForm, PurchaseUpdateForm
 
-# from django.urls import reverse_lazy
+from django.urls import reverse_lazy
 # from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -25,40 +25,39 @@ class IngredientList(ListView):
 class PurchaseList(ListView):
     model = Purchase
 
+class RecipeCreate(LoginRequiredMixin, CreateView):
+  model = Recipe
+  template_name = "vetoffice/Recipe_create_form.html"
+  form_class = RecipeCreateForm
 
-# class RecipeCreate(LoginRequiredMixin, CreateView):
-#   model = Recipe
-#   template_name = "vetoffice/Recipe_create_form.html"
-#   form_class = RecipeCreateForm
+class IngredientCreate(LoginRequiredMixin, CreateView):
+  model=Ingredient
+  template_name = "vetoffice/Ingredient_create_form.html"
+  form_class = IngredientCreateForm
 
-# class IngredientCreate(LoginRequiredMixin, CreateView):
-#   model=Ingredient
-#   template_name = "vetoffice/Ingredient_create_form.html"
-#   form_class = IngredientCreateForm
+class PurchaseCreate(LoginRequiredMixin, CreateView):
+  model=Purchase
+  template_name = "vetoffice/Purchase_create_form.html"
+  form_class = PurchaseCreateForm
 
-# class PurchaseCreate(LoginRequiredMixin, CreateView):
-#   model=Purchase
-#   template_name = "vetoffice/Purchase_create_form.html"
-#   form_class = PurchaseCreateForm
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
-#   def form_valid(self, form):
-#     form.instance.user = self.request.user
-#     return super().form_valid(form)
+class RecipeUpdate(LoginRequiredMixin, UpdateView):
+  model = Recipe
+  template_name = "vetoffice/Recipe_update_form.html"
+  form_class = RecipeUpdateForm
 
-# class RecipeUpdate(LoginRequiredMixin, UpdateView):
-#   model = Recipe
-#   template_name = "vetoffice/Recipe_update_form.html"
-#   form_class = RecipeUpdateForm
+class IngredientUpdate(LoginRequiredMixin, UpdateView):
+  model = Ingredient
+  template_name = "vetoffice/Ingredient_update_form.html"
+  form_class = IngredientUpdateForm
 
-# class IngredientUpdate(LoginRequiredMixin, UpdateView):
-#   model = Ingredient
-#   template_name = "vetoffice/Ingredient_update_form.html"
-#   form_class = IngredientUpdateForm
-
-# class PurchaseUpdate(LoginRequiredMixin, UpdateView):
-#   model = Purchase
-#   template_name = "vetoffice/Purchase_update_form.html"
-#   form_class = PurchaseUpdateForm
+class PurchaseUpdate(LoginRequiredMixin, UpdateView):
+  model = Purchase
+  template_name = "vetoffice/Purchase_update_form.html"
+  form_class = PurchaseUpdateForm
 
 class RecipeDelete(LoginRequiredMixin, DeleteView):
   model = Recipe
